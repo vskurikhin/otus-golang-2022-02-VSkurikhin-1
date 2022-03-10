ecfeb4463a3572498f1b8175dbce7bf41ec4b10c
Victor N. Skurikhin <vs@lnhost.net>
2022-03-10 16:36:14 +0300

    Дополнительное задание: поддержка экранирования через \

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,6 +9,8 @@ import (
 
 const EMPTY = ""
 
+const BACKSLASH = rune('\\')
+
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
@@ -22,15 +24,26 @@ func Unpack(s string) (string, error) {
 	return result, err
 }
 
-func unpackRuneLetter(r rune, runes []rune, i *uint64) (string, error) {
-	if unicode.IsDigit(r) {
+func unpackRuneLetter(current rune, runes []rune, i *uint64) (string, error) {
+	if unicode.IsDigit(current) {
 		return EMPTY, ErrInvalidString
 	}
-	result := string(r)
+	result := string(current)
 	head, tail := splitToHeadAndTail(runes, i)
 	if head != nil {
+		if BACKSLASH == current {
+			var err error
+			result, err = unpackRuneBackslash(*head)
+			if err != nil {
+				return EMPTY, err
+			}
+			head, tail = splitToHeadAndTailBackslashNumber(&current, head, tail, i)
+			if nil == head {
+				return result, nil
+			}
+		}
 		if unicode.IsDigit(*head) {
-			result = unpackRuneNumber(r, *head)
+			result = unpackRuneNumber(current, *head)
 			head, tail = splitToHeadAndTail(tail, i)
 			if nil == head {
 				return result, nil
@@ -45,9 +58,27 @@ func unpackRuneLetter(r rune, runes []rune, i *uint64) (string, error) {
 	return result, nil
 }
 
+func unpackRuneBackslash(r rune) (string, error) {
+	if !unicode.IsDigit(r) && r != BACKSLASH {
+		return EMPTY, ErrInvalidString
+	}
+	return string(r), nil
+}
+
 func unpackRuneNumber(r rune, p rune) string {
 	n, _ := strconv.Atoi(string(p))
 	return strings.Repeat(string(r), n)
+}
+
+func splitToHeadAndTailBackslashNumber(r *rune, head *rune, tail []rune, i *uint64) (*rune, []rune) {
+	h, t := splitToHeadAndTail(tail, i)
+	if nil == h {
+		return nil, nil
+	}
+	if unicode.IsDigit(*h) {
+		*r = *head
+	}
+	return h, t
 }
 
 func splitToHeadAndTail(runes []rune, i *uint64) (*rune, []rune) {
